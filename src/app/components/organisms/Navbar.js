97373c62a09c7017bc0c3420cb94066ef4117261
achto01 <ramadhikaptr@gmail.com>
2023-12-25 21:54:03 +0700

    ngerubah di navbar sama page

--- src/app/components/organisms/Navbar.js
+++ src/app/components/organisms/Navbar.js
@@ -7,7 +7,26 @@ const Navbar = () => {
       <NavItem text={'Home'} link={'#'} />
       <NavItem text={'Home'} link={'#'} />
       <NavItem text={'Home'} link={'#'} />
+      <div className="flex items-center space-x-2">
+      <input
+        type="text"
+        placeholder="Search here..."
+        className="py-1 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+      />
+      <button
+        
+        className="px-5 py-1 text-black bg-white rounded-md hover:bg-blue-600 focus:outline-none border shadow-lg"
+      >
+        Search
+      </button>
+    </div>
+    <div className="container mx-auto my-auto">
+      <div className="flex justify-end items-center">
+        <p> nanti disini userlink</p>
+      </div>
+    </div>
     </nav>
+    
   );
 };
 
